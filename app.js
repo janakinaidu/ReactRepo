dba6cbd92ff00fe98cfe4a18d328d2abe5bdf394
janakinaidu <janakinaidu493@gmail.com>
2018-10-04 20:17:57 +0530

    code changes for combination of state and props
    Some content of code is added for stateAndProps application
    
    Co-Authored-By: bkadime <bkadime@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,12 +1,48 @@
 import React from 'react';
 
 class App extends React.Component {
+	
+	constructor(props){
+		super(props);
+		this.state={
+			 header: "Header from props...",
+			 content: "Content from props..."
+		}
+	}
+	
     render() {
+		
+		var myStyle = {
+         fontSize: 100,
+         color: '#FF0000'
+        }
+		
         return (
             <div>
-            Hello World!!! from dev
+			<h1 style={myStyle}>Displaying some Text with styles</h1>
+            <Header headerProp = {this.state.header}/>
+            <Content contentProp = {this.state.content}/>
         </div>
     );
     }
+}
+class Header extends React.Component{
+	render(){
+		return (
+			<div>
+				<h1>{this.props.headerProp}</h1>
+			</div>
+		
+		);
+	}
+}
+class Content extends React.Component{
+	render(){
+		return (
+			<div>
+				<h1>{this.props.contentProp}</h1>
+			</div>
+		);
+	}
 }
 export default App;
