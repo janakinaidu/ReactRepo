b03a5e58525b50d758d4d76603c187f558c19f34
janakinaidu <janakinaidu493@gmail.com>
2018-11-08 23:22:06 +0530

    Update app.js
    Begining step for reqct-quicstart app

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-            Hello World!!! from dev
+            Hello World!!! from React-quickstart
         </div>
     );
     }
