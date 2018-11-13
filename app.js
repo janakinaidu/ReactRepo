91451e1af301c0c73ee613fe0561fa66a17c10ca
janakinaidu <janakinaidu493@gmail.com>
2018-11-13 20:43:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ class App extends React.Component {
     render() {
         return (
             <div>
-            Hello World!!!
-        </div>
+            Hello Welcome to ReactJS Standard App's.
+			</div>
     );
     }
 }
