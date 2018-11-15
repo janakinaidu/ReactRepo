f13594477a8fea1795bd866897a982447409efb9
janakinaidu <janakinaidu493@gmail.com>
2018-11-15 18:37:39 +0530

    Clock and Tick functionality App
    Clock and Tick functionality is added to the reactapppracticeapp2
    
    Co-Authored-By: bkadime <bkadime@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 import React from 'react';
 
-class App extends React.Component {
+class Clock extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { currentDate: new Date() };
+    }
+    componentDidMount() {
+        console.log("this statement was executed after DOM rendered by the Clock class");
+        this.timerID = setInterval(() => this.tick(), 1000);
+    }
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+        console.log("this statement was executed after DOM destroyed");
+    }
+    tick() {
+        this.setState({
+            currentDate: new Date()
+        });
+    }
+
     render() {
         return (
             <div>
-            Hello Welcome to  reactAppPracticeApp2.
-			</div>
-    );
+                <h1>Hello Welcome to  reactAppPracticeApp2.</h1>
+                <h2>It is {this.state.currentDate.toLocaleTimeString()}.</h2>
+            </div>
+        );
     }
 }
-export default App;
+export default Clock;
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js';
+import Clock from './App.js';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<Clock />, document.getElementById('app'));
