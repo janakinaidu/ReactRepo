7b3cdcd6464dc4b29d7299b55507f636968c97f8
janakinaidu <janakinaidu493@gmail.com>
2018-11-15 20:18:00 +0530

    code chagnes for importance of state
    importance of state is added

--- main.js
+++ main.js
@@ -2,4 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Clock from './App.js';
 
-ReactDOM.render(<Clock />, document.getElementById('app'));
+function App(){
+    return (
+        <div>
+            <Clock/>
+            <Clock/>
+            <Clock/>
+        </div>
+    );
+}
+//Here we can see the importance of state and aswell as we can treat every javascript functioni
+//is also a Component;
+
+ReactDOM.render(<App />, document.getElementById('app'));
